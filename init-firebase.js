b99967098ced7b54d626a10f5698e36acc73f8fb
nicoamigoni <nicolo.amigoni@mail.polimi.it>
2024-07-24 22:29:03 +0200

    defer

---
+++ init-firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDlKYfKl6b2tCXjJYP3raQIEW0lMatcDHk",
+    authDomain: "giocovacanzina.firebaseapp.com",
+    databaseURL: "https://giocovacanzina-default-rtdb.firebaseio.com",
+    projectId: "giocovacanzina",
+    storageBucket: "giocovacanzina.appspot.com",
+    messagingSenderId: "807834218186",
+    appId: "1:807834218186:web:d3bc04429069cbf4a34e33",
+    measurementId: "G-X9R10EH2K6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDlKYfKl6b2tCXjJYP3raQIEW0lMatcDHk",
-    authDomain: "giocovacanzina.firebaseapp.com",
-    databaseURL: "https://giocovacanzina-default-rtdb.firebaseio.com",
-    projectId: "giocovacanzina",
-    storageBucket: "giocovacanzina.appspot.com",
-    messagingSenderId: "807834218186",
-    appId: "1:807834218186:web:d3bc04429069cbf4a34e33",
-    measurementId: "G-X9R10EH2K6"
-  };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 
 // Define the folder path
